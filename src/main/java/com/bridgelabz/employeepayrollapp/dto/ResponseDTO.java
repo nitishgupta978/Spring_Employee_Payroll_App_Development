fa036c390114ceb2f00190491e736d7c8112e78a
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-03 15:11:29 +0530

    modify:getter setter method to replace @Data in ResponseDTO Class:

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,15 +1,21 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.Data;
+
 /**
  * @author :John,
  * date :03:06:2022
+ *
+ * UC1 for lombok
  */
-public class ResponseDTO {
+public @Data class ResponseDTO {
     private String message;
     private Object data;
-
     /**
+     *use @Data lombok Annotation it is combination of @code @Getter @Setter @RequiredArgsConstructor
      *
+     * @ToString @EqualsAndHashCode and take the that method automatically no need to provide this
+     * type of method  when we use @Data
      * @param message ,Create one ResponseDTO class for display only message and data on your user
      * @param data
      */
@@ -22,22 +28,5 @@ public class ResponseDTO {
      * create one constructor methode
      * @return message
      */
-    public String getMessage() {
-        return message;
-    }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-    /**
-     * create getter setter methode
-     */
 }
