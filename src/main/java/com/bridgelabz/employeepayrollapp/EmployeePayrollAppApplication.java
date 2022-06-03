ac3e1dec1deca14d9fa87517f50f3609de9713b6
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-03 16:12:39 +0530

    added:Use Lombok for Logging using @Slf4j and application properties

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,16 +11,35 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+/**
+ * @EnableSwagger2 use for Swagger
+ */
+@Slf4j
+/**
+ * Using Lombok for Logging ,Using just @Slf4j is enough to start using
+ * the Logger
+ */
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee payroll APP Started");
+
+		/**
+		 * return Employee payroll APP Started when complete the run configuration
+		 */
 	}
 	@Bean
 	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
+		 return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.employeepayrollapp"))
 				.build();
+		/**
+		 * use @Bean is methode-level-annotation
+		 * we create one method productApi and use Docket to connect our project between basePackage and  Swagger
+		 * when provide url on browser http://localhost:8080/swagger-ui.html#/
+		 */
+
 	}
 }
