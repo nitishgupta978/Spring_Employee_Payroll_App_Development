03150918b3308550db2b6d90e40b29c7c6250e98
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-05 11:00:12 +0530

    added:exception subPackage, class EmployeePayrollExceptonHandler

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**@author Nitishkumar gupta
+ * date:05-06-2022
+ * EmployeePayrollExceptionHandler.java
+ */
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
+        List<String> errorMessage=errorList.stream().map(objectError -> objectError.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDTO responseDTO= new ResponseDTO("Exception while processing Rest Request",errorMessage);
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+        /**
+         * using stream api method in Exception handling
+         * This is at a global level means you only have one '@ControllerAdvice' for each application And it will intercept the
+         * exceptions thrown by the controllers.
+         */
+    }
+
+
+
+}
